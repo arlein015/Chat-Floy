0135a18fb54e35558b1c830f91214f2d95d1eae9
arlein015 <rachehua45@gmail.com>
2026-03-24 01:45:20 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,26 @@
+// Importation des modules Firebase (Version SDK v10+)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
+// Ta configuration Firebase (Vérifie bien ces valeurs dans ta console Firebase)
 const firebaseConfig = {
-  apiKey: "AIzaSyD3cL4MokpURYKydVTugXArXC3-krQCAIc",
+  apiKey: "TON_API_KEY_ICI", 
   authDomain: "floy-2a96d.firebaseapp.com",
-  projectId: "Floy-2A96D",
-  storageBucket: "floy-2a96d.firebasestorage.app",
-  messagingSenderId: "360277317372",
-  appId: "1:360277317372:web:1b068a460ecc343022a979",
-  measurementId: "G-QV8DVCRY1Q"
+  projectId: "floy-2a96d",
+  storageBucket: "floy-2a96d.appspot.com",
+  messagingSenderId: "TON_SENDER_ID",
+  appId: "TON_APP_ID"
 };
 
+// Initialisation de l'application
 const app = initializeApp(firebaseConfig);
 
-// IL FAUT ABSOLUMENT CES LIGNES AVEC "export"
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+// Initialisation des services
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// Exportation pour les utiliser dans index.html et les autres modules
+export { auth, db, storage };
