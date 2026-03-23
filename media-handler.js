a6c272e7edbf12be94a11f4c0ca0d78f967acc83
arlein015 <rachehua45@gmail.com>
2026-03-24 00:54:48 +0300

    Update media-handler.js

--- media-handler.js
+++ media-handler.js
@@ -1,1 +1,23 @@
+import { storage } from './firebase-config.js';
+import { ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
+export const MediaHandler = {
+    async uploadImage(file, path, onProgress) {
+        return new Promise((resolve, reject) => {
+            const storageRef = ref(storage, `${path}/${Date.now()}_${file.name}`);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            uploadTask.on('state_changed', 
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    if (onProgress) onProgress(progress);
+                }, 
+                (error) => reject(error), 
+                async () => {
+                    const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+                    resolve(downloadURL);
+                }
+            );
+        });
+    }
+};
