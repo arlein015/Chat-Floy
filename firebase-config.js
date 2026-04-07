fa8f0837bac050d6009682447ad1b13d7c1a10c0
arlein015 <rachehua45@gmail.com>
2026-04-07 22:18:57 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,21 @@
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// On laisse ce bloc prêt à recevoir les VRAIES infos
 const firebaseConfig = {
-  apiKey: "VALEUR_A_COPIER",
-  authDomain: "VOTRE_PROJET.firebaseapp.com",
-  projectId: "VOTRE_PROJET_ID",
-  storageBucket: "VOTRE_PROJET.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdefgh"
+    apiKey: "METS_TA_CLE_ICI_SANS_ESPACES", 
+    authDomain: "VOTRE_PROJET.firebaseapp.com",
+    projectId: "VOTRE_PROJET_ID",
+    storageBucket: "VOTRE_PROJET.appspot.com",
+    messagingSenderId: "XXXXX",
+    appId: "XXXXX"
 };
 
-export const app = initializeApp(firebaseConfig);
+// Initialisation
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
+console.log("Connexion Firebase Floy : Initialisée 🚀");
