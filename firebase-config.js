3589bf03312be74e37afe4e32227a5b3f06f2796
arlein015 <rachehua45@gmail.com>
2026-04-07 22:09:40 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,26 +1,14 @@
-// Importation des modules Firebase (Version SDK v10+)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-// Ta configuration Firebase (Vérifie bien ces valeurs dans ta console Firebase)
 const firebaseConfig = {
-  apiKey: "TON_API_KEY_ICI", 
-  authDomain: "floy-2a96d.firebaseapp.com",
-  projectId: "floy-2a96d",
-  storageBucket: "floy-2a96d.appspot.com",
-  messagingSenderId: "TON_SENDER_ID",
-  appId: "TON_APP_ID"
+  apiKey: "AIzaSyD264JpNWs08p7exXOLRZdWUkbKBdviw8c",
+  authDomain: "floy-9f9a1.firebaseapp.com",
+  projectId: "floy-9f9a1",
+  storageBucket: "floy-9f9a1.firebasestorage.app",
+  messagingSenderId: "401063236997",
+  appId: "1:401063236997:web:ba53cf61961acd977b20ec",
+  measurementId: "G-YSGGQ4Q4TV"
 };
 
-// Initialisation de l'application
-const app = initializeApp(firebaseConfig);
-
-// Initialisation des services
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-// Exportation pour les utiliser dans index.html et les autres modules
-export { auth, db, storage };
+export const app = initializeApp(firebaseConfig);
